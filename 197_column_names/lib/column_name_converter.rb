91249fba164607d6fea80b47cf9cf17ec52afbad
Dominik Duda <gospodorz@gmail.com>
2016-07-18 13:15:32 +0200

    Fixed converting

--- 197_column_names/lib/column_name_converter.rb
+++ 197_column_names/lib/column_name_converter.rb
@@ -7,7 +7,7 @@ class ColumnNameConverter
 
   def excel_header
     num = []
-    num.unshift(add_next_digit) until column_number == 0
+    num.unshift(add_next_digit) until column_number <= 1
     num.join
   end
 
@@ -20,10 +20,21 @@ class ColumnNameConverter
   end
 
   def next_digit
-    corresponding_letter(column_number % 26)
+    last_digit? ? corresponding_letter(column_number) : corresponding_letter(column_number % 26)
+  end
+
+  def last_digit?
+    column_number <= 26
   end
 
   def corresponding_letter(number)
     (number + 64).chr
   end
+end
+
+column_name_converter = ColumnNameConverter.new
+input = File.open(ARGV[0]).readlines.map(&:to_i)
+input.each do |integer|
+  column_name_converter.column_number = integer
+  puts column_name_converter.excel_header
 end

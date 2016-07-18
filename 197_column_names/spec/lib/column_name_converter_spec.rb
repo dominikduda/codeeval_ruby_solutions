9d79db9cda8359bafb0d0acd79cf27e9debf7a2b
Dominik Duda <gospodorz@gmail.com>
2016-07-18 12:08:51 +0200

    Refactored spec for ColumnNameConverter

--- 197_column_names/spec/lib/column_name_converter_spec.rb
+++ 197_column_names/spec/lib/column_name_converter_spec.rb
@@ -2,13 +2,14 @@ require 'column_name_converter'
 
 describe ColumnNameConverter do
   let(:column_name_converter) { described_class.new(input) }
+  let(:perform) { column_name_converter.excel_header }
 
-  describe '#convert' do
+  describe '#excel_header' do
     context 'first column' do
       let(:input) { 1 }
 
       it 'returns correct header' do
-        expect(column_name_converter.excel_header).to eq 'A'
+        expect(perform).to eq 'A'
       end
     end
 
@@ -16,7 +17,7 @@ describe ColumnNameConverter do
       let(:input) { 3702 }
 
       it 'returns correct header' do
-        expect(column_name_converter.excel_header).to eq 'ELJ'
+        expect(perform).to eq 'ELJ'
       end
     end
 
@@ -24,7 +25,7 @@ describe ColumnNameConverter do
       let(:input) { 17_576 }
 
       it 'returns correct header' do
-        expect(column_name_converter.excel_header).to eq 'ZZZ'
+        expect(perform).to eq 'ZZZ'
       end
     end
   end
